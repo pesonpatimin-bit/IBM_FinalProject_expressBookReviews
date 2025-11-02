3f284269a54404759ea876a186e0d7e9a2981d0f
Peson Patimin <Peson.patmin@gmail.com>
2025-11-02 00:45:14 -0400

    task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,18 +20,48 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  //return res.status(300).json({message: "Yet to be implemented"});
+  let ISBNcode = req.params.isbn;
+  res.send(JSON.stringify({"ISBN" : ISBNcode}));
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const byAuthor = req.params.author;       // ดึงชื่อผู้แต่งจาก URL
+  const keys = Object.keys(books);          // เอา keys ของ object
+  const result = [];
+
+// iterate key, check author
+  keys.forEach(key => {
+    if (books[key].author.toLowerCase() === byAuthor.toLowerCase()) {
+      result.push(books[key]);
+    }
+  });
+
+  //res.send(JSON.stringify(result, null, 4));
+    res.json(result)
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
+  const byTitle = req.params.title;       // ดึงชื่อผู้แต่งจาก URL
+  const keys = Object.keys(books);          // เอา keys ของ object
+  const result = [];
+
+// iterate key, check title
+  keys.forEach(key => {
+    if (books[key].title.toLowerCase() === byTitle.toLowerCase()) {
+      result.push(books[key]);
+    }
+  });
+
+  //res.send(JSON.stringify(result, null, 4));
+    res.json(result)
+
+
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
