b03e80746cda40edbd2e36438153d4428bbda45f
Peson patimin <peson.patimin@gmail.com>
2025-11-05 10:19:54 -0500

    final

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
 
 public_users.post("/register", (req,res) => {
@@ -23,46 +24,65 @@ return res.status(404).json({message: "Unable to register user."});
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  //return res.status(300).json({message: "Yet to be implemented"});
-  res.send(JSON.stringify(books,null,4));
-});
+    //return res.status(300).json({message: "Yet to be implemented"});
+    //Creating a promise method. The promise will get resolved when timer times out after 6 seconds.
+      let myPromise = new Promise((resolve,reject) => {
+          resolve(JSON.stringify(books,null,4));
+      });
+    //Call the promise and wait for it to be resolved and then print a message.
+      myPromise.then((successMessage) => {
+          //console.log("From Callback " + successMessage)
+          res.send(successMessage);
+      })
+  })
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  //return res.status(300).json({message: "Yet to be implemented"});
-  let byIsbn = req.params.isbn;
-  res.send(JSON.stringify(books[byIsbn]));
+    //Write your code here
+    //return res.status(300).json({message: "Yet to be implemented"});
+      let byIsbn = req.params.isbn;
+      let myPromise = new Promise((resolve,reject) => {
+          resolve(JSON.stringify(books[byIsbn]));
+      });
+      //Call the promise and wait for it to be resolved and then print a message.
+      myPromise.then((successMessage) => {
+          //console.log("From Callback " + successMessage)
+          res.send(successMessage);
+      })
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author',async function (req, res) {
   //Write your code here
   //return res.status(300).json({message: "Yet to be implemented"});
-  const byAuthor = req.params.author;       // ดึงชื่อผู้แต่งจาก URL
-  const keys = Object.keys(books);          // เอา keys ของ object
-  const result = [];
-
-// iterate key, check author
-  keys.forEach(key => {
-    if (books[key].author.toLowerCase() === byAuthor.toLowerCase()) {
-      result.push(books[key]);
-    }
-  });
-
-  //res.send(JSON.stringify(result, null, 4));
-    res.json(result)
+    const byAuthor = req.params.author;   
+    const result = [];
+    try{
+        const keys = Object.keys(books);
+            // iterate key, check author
+            keys.forEach(key => {
+                if (books[key].author.toLowerCase() === byAuthor.toLowerCase()) {
+                    result.push(books[key]);
+                }
+            });
+            res.json(result)
+            //console.log(JSON.parse(data));
+            //res.send(JSON.stringify(result, null, 4));
+        }catch(err){
+            console.error(err);
+            res.status(500).json({ message: "error" });
+        }
+    
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title',async function (req, res) {
   //Write your code here
   const byTitle = req.params.title;       // ดึงชื่อผู้แต่งจาก URL
-  const keys = Object.keys(books);          // เอา keys ของ object
+  const keys = await Object.keys(books);          // เอา keys ของ object
   const result = [];
 // iterate key, check title
-  keys.forEach(key => {
+keys.forEach(key => {
     if (books[key].title.toLowerCase() === byTitle.toLowerCase()) {
       result.push(books[key]);
     }
@@ -77,7 +97,6 @@ public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
   const byIsbn = req.params.isbn;       // ดึงชื่อผู้แต่งจาก URL
   const keys = Object.keys(books);          // เอา keys ของ object
-  const result = [];
 // iterate key, check title
   keys.forEach(key => {
     if (key=byIsbn) {
@@ -87,8 +106,6 @@ public_users.get('/review/:isbn',function (req, res) {
   });
   //res.send(JSON.stringify(result, null, 4));
     //res.json(result)
-
-  
   //return res.status(300).json({message: "Yet to be implemented"});
 });
 
